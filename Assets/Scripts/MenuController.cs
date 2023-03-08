380e9181a0c165a1b858738d7898181e951e290d
SamuelT95 <samuel_tjahjadi@hotmail.com>
2023-03-08 14:13:21 -0800

    added a menu screen

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject menuPanel;
+
+    void Update()
+    {
+        Debug.Log("MenuController pushing updates");
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("Menu opened");
+            menuPanel.SetActive(!menuPanel.activeSelf);
+        }
+    }
+}
