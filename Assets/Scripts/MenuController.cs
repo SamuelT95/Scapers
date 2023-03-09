eb35c460eb6862a7ecef69dad8b2590b1b62e3f2
SamuelT95 <samuel_tjahjadi@hotmail.com>
2023-03-08 16:50:43 -0800

    fixed buttons appearance when pressing esc

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class MenuController : MonoBehaviour
 {
     public GameObject menuPanel;
+    public GameObject quitButton;
+    public GameObject continueButton;
 
     void Update()
     {
-
-
         //Debug.Log("MenuController pushing updates");
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             Debug.Log("Menu opened");
             menuPanel.SetActive(!menuPanel.activeSelf);
+            quitButton.SetActive(!quitButton.activeSelf);
+            continueButton.SetActive(!continueButton.activeSelf);
+
         }
     }
 }
