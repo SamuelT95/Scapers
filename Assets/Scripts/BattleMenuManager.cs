ed44c0a5fee82b465f63eb75e3cf51f7423a683d
traillio <tlyall2@my.bcit.ca>
2023-03-25 11:15:40 -0700

    Made buttons dynamic, made it so we only need one instance of menuManager

--- Assets/Scripts/BattleMenuManager.cs
+++ Assets/Scripts/BattleMenuManager.cs
@@ -1,16 +1,78 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BattleMenuManager : MonoBehaviour
 {
-/*    GameObject player = GameObject.FindGameObjectWithTag("Player");
-    GameObject enemy = GameObject.FindGameObjectWithTag("Enemy");*/
+    
+    //GameObject enemy = GameObject.FindGameObjectWithTag("Enemy");*/
 
-    public GameObject BattleMenu;
-    public GameObject AttackMenu;
-    public GameObject PhysicalAttackMenu;
-    public GameObject MagicAttackMenu;
+    private GameObject BattleMenu;
+    private GameObject AttackMenu;
+    private GameObject PhysicalAttackMenu;
+    private GameObject MagicAttackMenu;
+
+    Player player;
+
+    public void Start()
+    {
+        //get all buttons
+        Button attack = GameObject.Find("ButtonAttack").GetComponent<Button>();
+        Button run = GameObject.Find("ButtonRun").GetComponent<Button>();
+        Button heal = GameObject.Find("ButtonHeal").GetComponent<Button>();
+        Button physical = GameObject.Find("ButtonPhysical").GetComponent<Button>();
+        Button magical = GameObject.Find("ButtonMagic").GetComponent<Button>();
+
+        GameObject[] physButtons = GameObject.FindGameObjectsWithTag("physical_buttons");
+        GameObject[] magButtons = GameObject.FindGameObjectsWithTag("magical_buttons");
+        GameObject[] backButtons = GameObject.FindGameObjectsWithTag("back_buttons");
+
+        //initialise our player
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+
+
+        //assign functions for single buttons
+        attack.onClick.AddListener(Attack);
+        run.onClick.AddListener(Run);
+        heal.onClick.AddListener(Heal);
+        physical.onClick.AddListener(Physical);
+        magical.onClick.AddListener(Magical);
+
+        //get individual menus
+        BattleMenu = GameObject.Find("BattleMenu");
+        AttackMenu = GameObject.Find("AttackMenu");
+        PhysicalAttackMenu = GameObject.Find("PhysicalAttackMenu");
+        MagicAttackMenu = GameObject.Find("MagicalAttackMenu");
+
+
+        //set listeners for back buttons
+        for (int i = 0; i < backButtons.Length; i++)
+        {
+            backButtons[i].GetComponent<Button>().onClick.AddListener(Back);
+        }
+
+        //set text of attack buttons and apply listeners
+        for (int i = 0; i < physButtons.Length; i++)
+        {
+            //set text of buttons to their respective names
+            magButtons[i].GetComponent<Button>().GetComponentInChildren<TextMeshProUGUI>().text = player.magicalAttacks[i].name;
+            physButtons[i].GetComponent<Button>().GetComponentInChildren<TextMeshProUGUI>().text = player.physicalAttacks[i].name;
+
+            //if you use i directly, it will create a reference of i and when the function is called later it will always be the last value it was
+            int num = i;
+
+            magButtons[i].GetComponent<Button>().onClick.AddListener(() => MagAttack(num));
+            physButtons[i].GetComponent<Button>().onClick.AddListener(() => PhysAttack(num));
+        }
+
+        //disable all but one menu
+        AttackMenu.SetActive(false);
+        PhysicalAttackMenu.SetActive(false);
+        MagicAttackMenu.SetActive(false);
+    }
 
     public void Attack()
     {
@@ -20,21 +82,20 @@ public class BattleMenuManager : MonoBehaviour
 
     public void Back()
     {
-        if (BattleMenu)
-        {
-            BattleMenu.SetActive(true);
-        }
-        if (AttackMenu)
+        if (AttackMenu.activeSelf)
         {
             AttackMenu.SetActive(false);
+            BattleMenu.SetActive(true);
         }
-        if (PhysicalAttackMenu)
+        if (PhysicalAttackMenu.activeSelf)
         {
             PhysicalAttackMenu.SetActive(false);
+            AttackMenu.SetActive(true);
         }
-        if (MagicAttackMenu)
+        if (MagicAttackMenu.activeSelf)
         {
             MagicAttackMenu.SetActive(false);
+            AttackMenu.SetActive(true);
         }
     }
 
@@ -54,53 +115,19 @@ public class BattleMenuManager : MonoBehaviour
         PhysicalAttackMenu.SetActive(true);
     }
 
-    public void Slash()
-    {
-        Debug.Log("Slashing damage");
-        
-        Back();
-    }
-
-    public void Crush()
-    {
-        Debug.Log("Crushing damage");
-        Back();
-    }
-
-    public void Pierce()
-    {
-        Debug.Log("Piercing damage");
-        Back();
-    }
-
     public void Magical()
     {
         AttackMenu.SetActive(false);
         MagicAttackMenu.SetActive(true);
     }
 
-    public void Air()
+    public void PhysAttack(int buttonNo)
     {
-        Debug.Log("Air damage");
-        Back();
-    }
-    public void Electric()
-    {
-        Debug.Log("Electric damage");
-        Back();
+        print(player.physicalAttacks[buttonNo].name);
     }
 
-    public void Water()
+    public void MagAttack(int buttonNo)
     {
-        Debug.Log("Water damage");
-        Back();
+        print(player.magicalAttacks[buttonNo].name);
     }
-
-    public void Fire()
-    {
-        Debug.Log("Fire damage");
-        Back();
-    }
-
-
 }
